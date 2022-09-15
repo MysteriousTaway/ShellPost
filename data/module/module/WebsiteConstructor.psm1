1be2a2bb4ddd02d83a354ceaca931c6009cd8874
Jakub Jeřábek <mysterioustaway@outlook.com>
2022-09-15 20:14:18 +0200

    Made some changes in websiteconstructor

--- data/module/module/WebsiteConstructor.psm1
+++ data/module/module/WebsiteConstructor.psm1
@@ -10,19 +10,44 @@ function ConstructHTML {
         [String] $HTML_Footer_Template_Path,
         # Modifiers:
         # Number of posts defines how many times the body should be repeated in the final webpage
-        [int] $NumberOfPosts,
-        # Example: [Name="kokot",Value="kokos"]
-        # What will this^ do ? it will find every word kokot and replace it with kokos
+        # Deprecated: (Reason: Can count number of posts by counting the ; char in $HTML_Body_Regex)
+        #[int] $NumberOfPosts,
+        # Example: Name="dog",Value="frog";
+        # What will this^ do ? it will find every word dog and replace it with frog
         [string] $HTML_Body_Regex,
         # Where is the result is supposed to be saved:
         [String] $Final_Page_Location
     )
     #Load templates from folder:
-    $js = GetFromFile -RawUrl $JS_Template_Path -Surround "script"
-    $css = GetFromFile -RawUrl $CSS_Template_Path -Surround "style"
-    $html_header = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
-    $html_body = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
-    $html_footer = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
+    [String] $js = GetFromFile -RawUrl $JS_Template_Path -Surround "script"
+    [String] $css = GetFromFile -RawUrl $CSS_Template_Path -Surround "style"
+    [String] $html_header = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
+    [String] $html_body = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
+    [String] $html_footer = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
+    # Construct final
+    $FinalBody = ConstructFinalBody  -js $js -css $css -HeaderTemplate $html_header -BodyTemplate $html_body -FooterTemplate $html_footer -RegexString $HTML_Body_Regex
+    #TODO: REMOVE THIS!!:
+    return $FinalBody
+    #TODO: Add file saver if this shit works
+}
+
+function ConstructFinalBody{
+    param($js,$css,$HeaderTemplate,$BodyTemplate,$FooterTemplate,$RegexString)
+    # get number of bodies
+    [String] $Final
+    [int] $NumberOfPosts = CountCharacters -String $HTML_Body_Regex -Char ";"
+    [String[]] $Bodies
+    # Replaces name keywords with values and puts them together ... POWERSHELL PRE-PROCESSOR BABYYYYY!!
+    [String[]] $Names = ($RegexString -split "(?i).*?Name=`"(.*?)`"")
+    [String[]] $Values = ($RegexString -split "(?i).*?Value=`"(.*?)`"")
+    for ($i = 0; $i -lt $Names.Count; $i++) {
+        [String] $rx = "(?i).*?(" + $Names[$i] + ").*?"
+        [String] $add = $BodyTemplate -replace $rx, $Values[$i]
+        $Bodies = $Bodies + $add
+    }
+    # Put them together :D
+    [String] $return = $js + $css + $HeaderTemplate + [String]$Bodies + $FooterTemplate
+    return $return
 }
 
 function GetFromFile {
@@ -34,6 +59,11 @@ function GetFromFile {
         $return = $raw
     }
     return $return
+}
+
+function CountCharacters {
+    param ($String, $Char)
+    return ($String.ToCharArray() | Where-Object {$_ -eq $Char} | Measure-Object).Count
 }
 
 Export-ModuleMember -Function ConstructHTML
--- web.ps1
+++ web.ps1
@@ -3,3 +3,6 @@
 
 # To import a module it has to be linked in manifest as root and then imported as this:
 Import-Module .\data\module\manifest\WebsiteConstructor.psd1 -Force
+# Testing
+$html = ConstructHTML -JS_Template_Path "./data/templates/Main.js" -CSS_Template_Path "./data/templates/Main.css" -HTML_Header_Template_Path "./data/templates/MainHeader.html" -HTML_Body_Template_Path "./data/templates/MainBody.html" -HTML_Footer_Template_Path "./data/templates/MainFooter.html" -HTML_Body_Regex "Name=`"cow`",Value=`"rabbit`";Name=`"cow`",Value=`"frog`";"
+Write-Host $html
