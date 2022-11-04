c92d6ae00ece78453c23d8986e14b29c37aaa79a
Jakub Jeřábek <mysterioustaway@outlook.com>
2022-11-04 07:41:15 +0100

    rewrote a fair bit

--- data/module/module/WebsiteConstructor.psm1
+++ data/module/module/WebsiteConstructor.psm1
@@ -1,57 +1,22 @@
 function ConstructHTML {
     [CmdletBinding()]
     param(
-        # Style and functionality (Location):
         [Parameter(Mandatory=$true)]
-        [String] $JS_Template_Path,
+        [string]$Title,
         [Parameter(Mandatory=$true)]
-        [String] $CSS_Template_Path,
-        # Where is the result is supposed to be saved:
+        [string]$Username,
         [Parameter(Mandatory=$true)]
-        [String] $Final_Page_Location,
-        [Parameter(Mandatory=$true)]
-        [String] $PageType,
-        # HTML parts to be loaded:
-        # HTML file location:
-        [Parameter(Mandatory=$true)]
-        [String] $HTML_Template_Path,
-        # Modifiers:
-        # Values to be replaced in multi-mode for main page:
-        [String[]] $Values
+        [string]$Text
     )
-    #Load templates from folder:
-    [String] $js = GetFromFile -RawUrl $JS_Template_Path -Surround "script"
-    [String] $css = GetFromFile -RawUrl $CSS_Template_Path -Surround "style"
-    [String] $html = GetFromFile -RawUrl $HTML_Template_Path -Surround ""
-    #[String] $html_header = GetFromFile -RawUrl $HTML_Header_Template_Path -Surround ""
-    #[String] $html_body = GetFromFile -RawUrl $HTML_Body_Template_Path -Surround ""
-    #[String] $html_footer = GetFromFile -RawUrl $HTML_Footer_Template_Path -Surround ""
-    if ($PageType -eq "Multi") {
-        # Construct final multi
-        [String] $FinalBody = ConstructFinalBody_Multi  -js $js -css $css -HTMLTemplate $html -Values $Values
-    } else {
-        # Load main page:
-        [String] $FinalBody = GetFromFile -RawUrl $HTML_Template_Path -Surround ""
-    }
-    
-    # Save to file
-    $FinalBody | Out-File -FilePath $Final_Page_Location -Encoding utf8 -Verbose 
-}
 
-function ConstructFinalBody_Multi{
-    param($js,$css,$HTMLTemplate,$Values)
-    # Split html template into header, body and footer using regex
-    [String[]] $HTMLTemplateSplit = $HTMLTemplate -split "<!--SPLIT-->"
-    # Replace values in body from values array
-    for ($i = 0; $i -lt $Values.Count; $i = $i + 2) {
-        [String] $add = $HTMLTemplateSplit[1] -replace $Values[$i], $Values[$i+1]
-        # Verbose debug
-        Write-Host "[DEBUG: WebsiteConstructor, ConstructFinalBody_Multi] Replaced $($Values[$i]) with $($Values[$i+1])" -ForegroundColor Yellow
-        $Bodies = $Bodies + $add
-    }
-    [String] $return = $js + $css + $HTMLTemplateSplit[0] + [String]$Bodies + $HTMLTemplateSplit[2]
-    # Write-Host "[DEBUG: WebsiteConstructor, ConstructFinalBody_Multi] Final html: $return" -ForegroundColor Yellow
-    return $return
+    $HTML += GetFromFile -RawUrl "/data/template/page.html"
+    $HTML = $HTML.Replace("{{title}}", $Title)
+    $HTML = $HTML.Replace("{{username}}", $Username)
+    $HTML = $HTML.Replace("{{text}}", $Text)
+    $HTML += GetFromFile -RawUrl "/data/template/style.css" -Surround "style"
+    $HTML += GetFromFile -RawUrl "/data/template/script.js" -Surround "script"
+    
+    return $HTML
 }
 
 function GetFromFile {
@@ -65,9 +30,5 @@ function GetFromFile {
     return $return
 }
 
-#function CountCharacters {
-#    param ($String, $Char)
-#    return ($String.ToCharArray() | Where-Object {$_ -eq $Char} | Measure-Object).Count
-#}
-
+Export-ModuleMember -Function GetFromFile
 Export-ModuleMember -Function ConstructHTML
--- web.ps1
+++ web.ps1
@@ -2,12 +2,11 @@
 #New-ModuleManifest .\data\module\manifest\ManifestName.psd1
 # To import a module it has to be linked in manifest as root and then imported as this:
 Import-Module ".\data\module\manifest\WebsiteConstructor.psd1" -Force -Verbose
-Import-Module ".\data\module\manifest\RequestHandler.psd1" -Force -Verbose
 Import-Module ".\data\module\manifest\SQLManager.psd1" -Force -Verbose
-# Import manifest:
-$configJson = Get-Content "data/manifest.json"
-# Convert to object
-$configObject = $configJson | ConvertFrom-Json
+## Import manifest:
+#$configJson = Get-Content "data/manifest.json"
+## Convert to object
+#$configObject = $configJson | ConvertFrom-Json
 $run = $true
 $ip = "127.0.0.1"
 $port = "8080"
@@ -29,7 +28,7 @@ while ($run) {
         # We can log the request to the terminal
         write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
         
-        [string]$html = HandleGET -RawURL $context.Request.RawUrl -jsonManifest $configObject
+        [string]$html = GetFromFile -RawUrl "data/static/main.html" -Surround ""
         
         #responded to the request
         $buffer = [System.Text.Encoding]::UTF8.GetBytes($html) # convert html to bytes
@@ -38,28 +37,40 @@ while ($run) {
         $context.Response.OutputStream.Close() # close the response
     }
 
-    # http://127.0.0.1/some/post'
-    # if ($context.Request.HttpMethod -eq 'POST' -and $context.Request.RawUrl -eq '/some/post') {
-    # 
-    #     # decode the form post
-    #     # html form members need 'name' attributes as in the example!
-    #     $FormContent = [System.IO.StreamReader]::new($context.Request.InputStream).ReadToEnd()
-    # 
-    #     # We can log the request to the terminal
-    #     write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
-    #     Write-Host $FormContent -f 'Green'
-    # 
-    #     # the html/data
-    #     [string]$html = "<h1>A Powershell Web server</h1><p>Post Successful!</p>" 
-    # 
-    #     #responded to the request
-    #     $buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
-    #     $context.Response.ContentLength64 = $buffer.Length
-    #     $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
-    #     $context.Response.OutputStream.Close() 
-    # }
+    if ($context.Request.HttpMethod -eq 'POST' -and $context.Request.RawUrl -eq '/') {
+        $FormContent = [System.IO.StreamReader]::new($context.Request.InputStream).ReadToEnd()
+    
+        # We can log the request to the terminal
+        write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
+        Write-Host $FormContent -f 'Green'
+
+        $Title = $FormContent.Split("&")[0].Split("=")[1]
+        $Username = $FormContent.Split("&")[1].Split("=")[1]
+        $Text = $FormContent.Split("&")[2].Split("=")[1]
+        # the html/data
+        [string]$html = ConstructHTML -Title $Title -Username $Username -Text $Text
+    
+        #responded to the request
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($html)
+        $context.Response.ContentLength64 = $buffer.Length
+        $context.Response.OutputStream.Write($buffer, 0, $buffer.Length)
+        $context.Response.OutputStream.Close() 
+    }
 
     if ($context.Request.HttpMethod -eq 'GET' -and $context.Request.RawUrl -eq '/taskkill') {
         $run = $false
+        
+        # We can log the request to the terminal
+        write-host "$($context.Request.UserHostAddress)  =>  $($context.Request.Url)" -f 'mag'
+        
+        [string]$html = "<h1> SERVER SHUTDOWN </h1>"
+        
+        #responded to the request
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($html) # convert html to bytes
+        $context.Response.ContentLength64 = $buffer.Length
+        $context.Response.OutputStream.Write($buffer, 0, $buffer.Length) #stream to browser
+        $context.Response.OutputStream.Close() # close the response
     }
 }
+# stop the server
+$http.Stop()
